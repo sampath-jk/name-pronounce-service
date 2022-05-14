cdc01a55cec7ef409e6409985ad81cb25938615f
Jagadeesh Sampatha Rao <sampath.jagadeesh@gmail.com>
2022-05-14 14:33:28 +0530

    Ai Services

---
+++ src/main/java/com/wf/hackathon/service/ai/Constants.java
@@ -0,0 +1,9 @@
+package com.wf.hackathon.service.ai;
+
+public class Constants
+{
+static final String SPEECH__SUBSCRIPTION__KEY = "1140d6c40d6a4f32bfa09d9270d1c509";
+static final String SPEECH__SERVICE__REGION = "eastus";
+static final String SPEECH_SOURCE_LANG="en-US";
+static final String SPEECH_TARGET_LANG="en";
+}
