29f62b1c30e9724430e0955267748b8c5fc96475
sunkavallivijay <42885023+sunkavallivijay@users.noreply.github.com>
2022-05-15 19:39:52 +0530

    OpenApi Doc addition

--- src/main/java/com/wf/hackathon/NamePronunciationApplication.java
+++ src/main/java/com/wf/hackathon/NamePronunciationApplication.java
@@ -1,8 +1,21 @@
 package com.wf.hackathon;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+		title = "Name Pronunciation Services",
+		version = "1.0",
+		description = "Name Pronunciation API"
+)
+)
+@SecurityScheme(name = "NamePronunciationApi", scheme = "Bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @SpringBootApplication
 public class NamePronunciationApplication {
 
--- src/main/java/com/wf/hackathon/controller/EmployeeController.java
+++ src/main/java/com/wf/hackathon/controller/EmployeeController.java
@@ -2,9 +2,11 @@ package com.wf.hackathon.controller;
 
 import com.wf.hackathon.model.EmployeeResponse;
 import com.wf.hackathon.model.EmployeeSignupRequest;
-import com.wf.hackathon.model.LoginRequest;
 import com.wf.hackathon.model.SuccessResponse;
 import com.wf.hackathon.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,26 +24,36 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    @Operation(method = "GET", summary = "Greet", description = "A generic greeting service")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "General greeting") })
     @GetMapping("/hello")
     public ResponseEntity<SuccessResponse> hello() {
         return new ResponseEntity(new SuccessResponse(200, "Success", "Hello"), HttpStatus.OK);
 
     }
 
+    @Operation(summary = "register", description = "Provides User registration")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Registration successful") })
     @PostMapping("/register")
     public ResponseEntity<SuccessResponse> register(@Valid @RequestBody EmployeeSignupRequest loginRequest) {
         employeeService.register(loginRequest);
         return new ResponseEntity(new SuccessResponse(200, "Success", "Hello"), HttpStatus.OK);
 
     }
-
+    @Operation(summary = "Get employee", description = "Provides employee information")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee information") })
     @GetMapping("/getEmployee/{id}")
     public ResponseEntity<SuccessResponse> getEmployee(@PathVariable String id) {
         EmployeeResponse employee = employeeService.getEmployee(id);
         return new ResponseEntity(new SuccessResponse( "Success", employee), HttpStatus.OK);
 
     }
-
+    @Operation(summary = "Employee search", description = "Provides employee name auto-suggest")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employees matching requested name") })
     @GetMapping("/employeeSearch/{name}")
     public ResponseEntity<SuccessResponse> employeeSearch(@PathVariable String name) {
         List<EmployeeResponse> allEmployees = employeeService.getAllEmployees(name);
