af813d4e59bfa67e0c821b88de945ab5beecb707
Jagadeesh Sampatha Rao <sampath.jagadeesh@gmail.com>
2022-05-18 02:39:23 +0530

    exceptions

--- src/main/java/com/wf/hackathon/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wf/hackathon/exception/GlobalExceptionHandler.java
@@ -6,17 +6,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NamePronounceException.class)
     public ResponseEntity<Object> handleNamePronounceException(NamePronounceException e) {
+        log.error(e.getLocalizedMessage());
+        e.printStackTrace();
         ErrorResponse response = new ErrorResponse("Exception while processing request");
         return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleOtherExceptions(Exception e) {
+        log.error(e.getLocalizedMessage());
+        e.printStackTrace();
         ErrorResponse response = new ErrorResponse("Exception while processing request");
         return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
