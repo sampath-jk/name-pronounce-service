26097bca6fff99313ef1b6258c7f4f9704194020
sunkavallivijay <42885023+sunkavallivijay@users.noreply.github.com>
2022-05-17 23:56:17 +0530

    - Remove status field

--- src/main/java/com/wf/hackathon/model/SuccessResponse.java
+++ src/main/java/com/wf/hackathon/model/SuccessResponse.java
@@ -7,24 +7,11 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SuccessResponse<T> {
 
     private String message;
-    private Integer statusCode;
     private T data;
-
-    public SuccessResponse(Integer statusCode, String message, T data){
-        this.statusCode= statusCode;
-        this.message = message;
-        this.data= data;
-    }
-
-    public SuccessResponse(Integer statusCode, String message){
-        this.statusCode= statusCode;
-        this.message = message;
-    }
 
     public SuccessResponse(String message, T data) {
         this.message = message;
